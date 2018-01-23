a0ed0f618b5e7219034f4316d900e133c0a73a58
sha5hw4t <shashwat_shetty@hotmail.com>
2018-01-23 04:27:59 -0500

    added comments in InsertionSort

--- algorithms/src/InsertionSort.java
+++ algorithms/src/InsertionSort.java
@@ -1,22 +1,39 @@
+/*
+Sorts a given integer array using the Merge Sort Algo.
+Pseudocode followed for the Algo is:
+    INSERTION-SORT(A)
+        for j = 2 to A.length
+            key = A[j]
+            i = j - 1
+            while i > 0 and A[i] > key
+                A[i + 1] = A[i]
+                i = i - 1
+            A[i + 1] = key
+ */
 public class InsertionSort {
-    public static int[] inertionSort(int[] input){
+    /*
+    Given: Unsorted array
+    Effect: Sorts the array in ascending order.
+     */
+    public static void inertionSort(int[] input){
+        // outer loop considers one element and checks for the correct position
+        // it should be placed in the inner loop
         for(int j = 1; j < input.length; j++){
-            int current = input[j];
+            int key = input[j];
             int i = j - 1;
-            while(i >= 0 && input[i] > current){
+            while(i >= 0 && input[i] > key){
                 input[i + 1] = input[i];
                 i--;
             }
-            input[i + 1] = current;
+            input[i + 1] = key;
         }
-        return input;
     }
 
     public static void main(String args[]){
         int[] arr1 = {3,5,7,2,1,4,6,9,0,8};
-        int[] result = InsertionSort.inertionSort(arr1);
+        InsertionSort.inertionSort(arr1);
         System.out.println("The Sorted Array is: ");
-        for(int e:result){
+        for(int e:arr1){
             System.out.print(e);
         }
     }
