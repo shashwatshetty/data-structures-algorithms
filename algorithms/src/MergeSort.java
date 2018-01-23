0a0d180e6ff84a0060008cfa62a32aa8d349d6b4
sha5hw4t <shashwat_shetty@hotmail.com>
2018-01-23 01:56:40 -0500

    adding comments to MergeSort

--- algorithms/src/MergeSort.java
+++ algorithms/src/MergeSort.java
@@ -28,24 +28,33 @@ Pseudocode followed for the Algo is:
 			MERGE(A,p,q,r)
  */
 public class MergeSort {
+    /*
+    Given: Original array, starting index p, breakpoint index q, end index r of a sub-problem.
+    Effect: merges the elements into a sorted array from index p to r
+    Invariant: p <= q <= r where arr[p:q] and arr[q+1:r] is sorted.
+     */
     public static void merge(int[] arr, int p, int q, int r){
-        int n1 = q - p + 2;
-        int n2 = r - q + 1;
+        int n1 = q - p + 2;             // size of left sorted array
+        int n2 = r - q + 1;             // size of right sorted array
         int[] left = new int[n1];
         int[] right = new int[n2];
-        int inf = Integer.MAX_VALUE;
+        int inf = Integer.MAX_VALUE;    // sentinel card value
+        // initialising left array
         for(int i = 1; i < n1; i++){
             left[i - 1] = arr[p + i - 1];
-
         }
+        // initialising right array
         for(int j = 1; j < n2; j++){
             right[j - 1] = arr[q + j];
         }
+        // initialising last value as sentinel
         left[n1 - 1] = inf;
         right[n2 - 1] = inf;
         int i = 0;
         int j = 0;
         for(int k = p; k <= r; k++){
+            // compare top cards of the two arrays, lowest value goes to the
+            // the original array from index p to r
             if(left[i] < right[j]){
                 arr[k] = left[i];
                 i++;
@@ -61,6 +70,8 @@ public class MergeSort {
     Effect: Sorts the array from index p to r in the original array arr.
      */
     public static void mergeSort(int[] arr, int p, int r){
+        // divide & conquer strategy used, problem divided into two parts
+        // and each part is solved recursively
         if(p < r){
             int q = (p + r)/2;
             mergeSort(arr, p, q);
