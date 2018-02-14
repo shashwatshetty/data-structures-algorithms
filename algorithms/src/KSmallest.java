53daa2c2decf75168fbab00d0d3b6e1fc9dfe68b
sha5hw4t <shashwat_shetty@hotmail.com>
2018-02-13 19:50:50 -0500

    added timing benchmark

--- algorithms/src/KSmallest.java
+++ algorithms/src/KSmallest.java
@@ -1,11 +1,15 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class KSmallest {
     /*
      Given:   an array of n integers and a non-negative integer k <= n,
      Returns: an array of length k containing the k smallest
                 values in the given array, in non-decreasing order.
-     Run Time: Big-O((k^2).n)
+     Run Time: Big-O(k.n)
    */
     public static int[] smallest(int[] input, int k){
         int[] res = new int[k];
@@ -34,18 +38,42 @@ public class KSmallest {
         return res;
     }
 
+    // benchmarking code to calculate the running time for the calculateLevenshtein method.
+    public static <T> int[] time (Supplier<T> thunk) {
+        System.out.println();
+        try {
+            long t0 = System.currentTimeMillis();
+            int[] result = (int[])thunk.get();
+            long t1 = System.currentTimeMillis();
+            long t = t1 - t0;
+            System.out.println ( "Program Runs in " + t + " milliseconds!");
+            return result;
+        }
+        catch (Error e) {
+            System.out.println ("\nERROR: " + e);
+            return null;
+        }
+    }
+
+    public static void printArray(int[] res){
+        for(int e : res) {
+            System.out.print(e + " ");
+        }
+    }
+
     public static void main(String args[]){
         Random r = new Random();
-        int[] test = new int[20];
-        for(int i = 0; i < 20; i++)
-            test[i] = r.nextInt(20);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Input Array Length");
+        int n = sc.nextInt();
+        System.out.println("Enter Value for k");
+        int k = sc.nextInt();
+        int[] test = new int[n];
+        for(int i = 0; i < n; i++)
+            test[i] = r.nextInt(Integer.MAX_VALUE);
         System.out.println("For input array: ");
-        for(int e : test)
-            System.out.print(e+" ");
-        System.out.println();
-        int[] res = smallest(test, 15);
-        System.out.println("The 15 smallest elements are: ");
-        for(int e : res)
-            System.out.print(e+" ");
+        printArray(test);
+        int[] res = time(() -> smallest(test, k));
+        printArray(res);
     }
 }
