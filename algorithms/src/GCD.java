8399fd575f71cb971b9595f8b2dd386c63dab582
sha5hw4t <shashwat_shetty@hotmail.com>
2018-03-19 17:43:11 -0400

    added gcd return, pseudocode edited

--- algorithms/src/GCD.java
+++ algorithms/src/GCD.java
@@ -29,18 +29,19 @@ public class GCD {
     }
 
     /*
-    Below pseudo-code is to calculate co-efficient for only one number
     Pseudocode:
-        INVERSE(a, b)
-            t = 0
-            newt = 1
-            r = b
-            newr = a
-            while newr != 0
-                quotient = floor(r/newr)
-                (t, newt) = (newt, t - quotient * newt)
-                (r, newr) = (newr, r - quotient * newr)
-            return t
+        EXTENDED-GCD(a, b)
+            s = 0   old_s = 1
+            t = 1   old_t = 0
+            r = b   old_r = a
+            while r != 0
+                quotient = floor(old_r/r)
+                (old_r, r) = (r, old_r - quotient * r)
+                (old_s, s) = (s, old_s - quotient * s)
+                (old_t, t) = (t, old_t - quotient * t)
+            output "Bézout coefficients:", (old_s, old_t)
+            output "greatest common divisor:", old_r
+            output "quotients by the gcd:", (t, s)
      */
 
     /*
@@ -81,7 +82,7 @@ public class GCD {
             t0 = t1;
             t1 = newT;
         }
-        BigInteger res[] = {s0, t0};
+        BigInteger res[] = {s0, t0, r0};
         return res;
     }
 
@@ -93,7 +94,9 @@ public class GCD {
         BigInteger[] coeffs = extendedEuclid(a, b);
         BigInteger x = coeffs[0];
         BigInteger y = coeffs[1];
+        BigInteger extendGcd = coeffs[2];
         System.out.println("The Co-Efficients of Bezout's Identity are: " + x + " " + y);
         System.out.println("The GCD Using Bezout's Identity is: " + a.multiply(x).add(b.multiply(y)));
+        System.out.println("The GCD returned by the Extended Euclidean Algorithm is: " + extendGcd);
     }
 }
